fe29fe67234003c72fd471e02371f7e6e171cdd8
Administrator <Administrator@DESKTOP-8SOEQRM>
2026-06-16 15:25:36 +0300

    Add custom domain-based authorization and policy updates
    Implemented DomainNameRequirement and handler for domain-based access control. Registered the handler in DI, enhanced JWT authentication with event logging, and added new authorization policies (AdminOnly, OnlyAuthenticated, DepartmentOnly, MinimumAge). Refactored using statements and improved code organization.
    
    Add custom domain-based authorization and policy updates
    
    Implemented DomainNameRequirement and handler for domain-based access control. Registered the handler in DI, added logging to JWT authentication events, and expanded authorization policies with role, claim, and custom assertion support. Cleaned up using directives.

---
+++ src/TaskFlow.API/Authorizations/DomainNameRequirementHandler.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace TaskFlow.API.Authorizations
+{
+
+    public class DomainNameRequirementHandler : AuthorizationHandler<DomainNameRequirement>
+    {
+        private readonly ILogger<DomainNameRequirementHandler> _logger;
+
+        public DomainNameRequirementHandler(ILogger<DomainNameRequirementHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, DomainNameRequirement requirement)
+        {
+
+            if(context.User.Identity.IsAuthenticated)
+            {
+                String authenticatedUserEmail = context.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+
+                if (String.IsNullOrEmpty(authenticatedUserEmail))
+                {
+                    _logger.LogError("Authorization failed: No email claim found for the authenticated user.");
+                    context.Fail();
+                    return;
+                }
+
+
+                if (authenticatedUserEmail.Contains(requirement.RequiredDomain))
+                {
+                    _logger.LogInformation("Authorization succeeded: Required domain matches the expected domain.");
+                    context.Succeed(requirement);
+                }
+                else
+                {
+                    _logger.LogError("Authorization failed: Required domain does not match the expected domain.");
+                    context.Fail();
+                }
+            }
+            else
+            {
+                context.Succeed(requirement);
+
+            
+            }
+
+            
+
+
+
+        }
+    }
+}
